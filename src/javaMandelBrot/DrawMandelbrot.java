32d4a44e120b285cc3e6259e1b772f8780e77955
kmannislands <kieranjarrettmann@gmail.com>
2016-03-09 14:57:59 -0800

    Small changes
    Still not a working version

--- src/javaMandelBrot/DrawMandelbrot.java
+++ src/javaMandelBrot/DrawMandelbrot.java
@@ -1,16 +1,26 @@
 package javaMandelBrot;
 
 import javafx.*;
+import java.io.*;
 import java.util.ArrayList;
 
 /**
- * Insert class header here
+ * This class accepts a File and Mandelbrot object and 
+ * implements runnable to draw a .png image to the specified
+ * file directory.
+ * 
+ * @authors: Kieran Mann and Seph Martin
+ * @email: kmann@ucsd.edu, //seph's email
+ * @version: 2.0
  */
-public class DrawMandelbrot implements Runnable {
+public class DrawMandelbrot {
+	// no need to thread this class as output has to join
+	// anyways, unless we have a way of writ
   
   private Mandelbrot FRAME;
   private int i;
   private int resolution;
+  private File outDir;
   
   private long startTime;
   private long endTime;
@@ -22,21 +32,30 @@ public class DrawMandelbrot implements Runnable {
    * @param canvas - the canvas to draw the arc in. Should be
    * provided by objectdraw
    */
-  public DrawMandelbrot(int i, DrawingCanvas canvas, Mandelbrot FRAME, int resolution) {
+  public DrawMandelbrot(int i, File outDir, Mandelbrot FRAME) {
     this.FRAME = FRAME;
-    this.canvas = canvas;
+    this.outDir = outDir;
     this.i = i;
     this.resolution = resolution;
   }
+  
+  /**
+   * 
+   * @param x
+   * @param y
+   * @param gray
+   */
 
   private void writePixel(double x, double y, Color gray) {
     FilledRect pixel = new FilledRect(x, y, 1.0, 1.0, canvas);
     pixel.setColor(gray);
   }
+  
   /**
    * Executed when the thread starts and runs indefinitly, moving the arc
    * across the screen.
    */
+  
   public void run() {
     startTime = System.nanoTime();
     Mandelbrot frameRender = FRAME;
